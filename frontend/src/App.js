8568cd1ce30e10d291e8ca830baaff887f0348fc
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2020-12-12 17:24:30 -0500

    Basic react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <>
+//     <h1> Welcome to pro shop</h1>
+//     </>
+//   );
+// }
+
+const App = () => {
+  return (
+    <>
+    <h1> Welcome to pro shop</h1>
+    </>
+  );
+}
+
+export default App;
